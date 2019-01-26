ab596af71b467117fa62abdda50229539db20116
Srdjan Cosic <sadde.95@hotmail.com>
2019-01-26 15:48:52 +0100

    Add ability to insert values on the board

--- src/com/srdjancosic/Game.java
+++ src/com/srdjancosic/Game.java
@@ -8,7 +8,7 @@ import com.srdjancosic.commands.ResetCommand;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Game {
+class Game {
     private static Scanner scanner = new Scanner(System.in);
     private GameBoard gameBoard;
 
@@ -36,8 +36,43 @@ public class Game {
     }
 
     private boolean awaitInput(GameBoard gameBoard) {
+        System.out.print("Enter a row index between 0-3: ");
+        if (scanner.hasNextInt()) {
+            int row = scanner.nextInt();
+            if (row < 1 || row > 3)
+                return awaitColumnInput(row);
+        }
         final String choice = scanner.next();
         choices.get(choice).call(gameBoard);
         return !choice.equals("help");
+    }
+
+    private boolean awaitColumnInput(int row) {
+        System.out.print("Enter a column index between 0-3: ");
+        if (scanner.hasNextInt()) {
+            int column = scanner.nextInt();
+            if (!this.gameBoard.indexesAreValid(row, column)) {
+                System.out.println("Those indexes are not valid. (They might be reserved)");
+                return false;
+            }
+            return this.awaitValueInput(row, column);
+        }
+        System.out.println("That value is not a number");
+        return false;
+    }
+
+    private boolean awaitValueInput(int row, int column) {
+        System.out.print("Enter a value between 1-9: ");
+        if (scanner.hasNextInt()) {
+            int value = scanner.nextInt();
+            if (!this.gameBoard.isValueValid(value)) {
+                System.out.println("That value is invalid");
+                return false;
+            }
+            this.gameBoard.insert(row, column, value);
+            return true;
+        }
+        System.out.println("That value is not a number");
+        return false;
     }
 }
--- src/com/srdjancosic/GameBoard.java
+++ src/com/srdjancosic/GameBoard.java
@@ -2,8 +2,15 @@ package com.srdjancosic;
 
 import java.util.Arrays;
 
-public class GameBoard {
+class GameBoard {
     private int[][] slots;
+
+    private int[][] reservedSlots = {
+        {0, 2},
+        {1, 1}, {1, 3},
+        {2, 0}, {2, 2},
+        {3, 1}
+    };
 
     GameBoard() {
         this.initializeBoard();
@@ -20,6 +27,23 @@ public class GameBoard {
 
     public void clear() {
         this.initializeBoard();
+    }
+
+    void insert(int row, int column, int value) {
+        this.slots[row][column] = value;
+    }
+
+    boolean indexesAreValid(int row, int column) {
+        if (row < 0 || row > 3) return false;
+        if (column < 0 || column > 3) return false;
+        for (int[] reservedSlot : this.reservedSlots) {
+            if (reservedSlot[0] == row && reservedSlot[1] == column) return false;
+        }
+        return true;
+    }
+
+    boolean isValueValid(int value) {
+        return 0 < value  && value < 10;
     }
 
     @Override
