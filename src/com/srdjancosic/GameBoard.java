77fd098d7902bea4d9a9dd74c39f89a366ee69f5
Srdjan Cosic <sadde.95@hotmail.com>
2019-01-26 13:50:17 +0100

    Add GameBoard class. Add toString method. Fix .gitignore

---
+++ src/com/srdjancosic/GameBoard.java
@@ -0,0 +1,30 @@
+package com.srdjancosic;
+
+import java.util.Arrays;
+
+public class GameBoard {
+    private int[][] slots;
+
+    GameBoard() {
+        this.initializeBoard();
+    }
+
+    private void initializeBoard() {
+        this.slots = new int[][]{
+            {0, 0, 3, 0},
+            {0, 2, 0, 1},
+            {1, 0, 2, 0},
+            {0, 3, 0, 0}
+        };
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.deepToString(
+            this.slots
+        ).replace("[[", ""
+        ).replaceAll("[\\[\\]]", System.lineSeparator()
+        ).replace("]]", ""
+        ).replace(",", "");
+    }
+}
--- src/com/srdjancosic/Main.java
+++ src/com/srdjancosic/Main.java
@@ -3,5 +3,7 @@ package com.srdjancosic;
 public class Main {
 
     public static void main(String[] args) {
+        GameBoard gameBoard = new GameBoard();
+        System.out.println(gameBoard.toString());
     }
 }
